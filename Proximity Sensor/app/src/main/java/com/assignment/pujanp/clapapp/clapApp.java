18d3cb9164d9b9a15057d2fd304fa13d53938498
pujan1 <pujan3892@gmail.com>
2016-03-25 23:08:06 -0700

    Proximity Sensor

---
+++ Proximity Sensor/app/src/main/java/com/assignment/pujanp/clapapp/clapApp.java
@@ -0,0 +1,70 @@
+package com.assignment.pujanp.clapapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class clapApp extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sm;
+    private Sensor proxy;
+    private MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clap_app);
+
+
+
+        player = MediaPlayer.create(this, R.raw.clap_sound);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proxy = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+
+        sm.registerListener(this, proxy, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_clap_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        player.start();
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        player.stop();
+
+    }
+}
